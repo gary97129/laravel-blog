ba94fb830f21fb34f26ce097936d20896feb8103
gray <40941118@gm.nfu.edu.tw>
2023-08-22 19:05:18 +0800

    DB取得資料

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,16 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function index(){
-        return view("posts.index");
+        $posts = Post::orderBy('updated_at', 'DESC')->get();
+
+        $data = [
+            "fist_post" => $posts->shift(),
+            "posts" => $posts,
+        ];
+
+        return view("posts.index", $data);
     }
 
     public function show(int $id){
-        return view("posts.show");
+        $post = Post::find($id);
+
+        $data = [
+          "post" => $post,
+        ];
+
+        return view("posts.show", $data);
     }
 }
 
