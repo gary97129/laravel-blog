e18957ff60769365d76a0dd849f667fc082333b8
gray <40941118@gm.nfu.edu.tw>
2023-08-22 12:18:08 +0800

    提取出的master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,21 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="description" content=""/>
     <meta name="author" content=""/>
-    <title>Heroic Features - Start Bootstrap Template</title>
+    <title> @yield('title') - My Blog </title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}"/>
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet"/>
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="{{ asset('css/home-styles.css') }}" rel="stylesheet"/>
+
+    @yield('page-styles')
 
 </head>
 <body>
+    @include('layouts.shared.navbar')
 
-<!-- Bootstrap core JS-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-<!-- Core theme JS-->
-<script src="{{ asset('js/scripts.js') }}"></script>
+    @yield('page-content')
+
+    @include('layouts.shared.footer')
+
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    @yield('page-scripts')
 
 </body>
 </html>
