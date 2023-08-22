ac8daf03ee8c721b637db3f30e4b061f909a52fa
gray <40941118@gm.nfu.edu.tw>
2023-08-22 19:08:26 +0800

    admin變更資料庫Controller

---
+++ app/Http/Controllers/AdminPostsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class AdminPostsController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('updated_at', 'DESC')->get();
+
+        $data = [
+          'posts' => $posts,
+        ];
+        return view('admin.posts.index', $data);
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        Post::create($request->all());
+
+        return redirect()->route('admin.posts.index');
+    }
+
+    public function edit(Post $post)
+    {
+        $data = [
+            "post" => $post,
+        ];
+
+        return view('admin.posts.edit', $data);
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $post->update($request->all());
+
+        return redirect()->route('admin.posts.index');
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('admin.posts.index');
+    }
+}
