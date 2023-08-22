6bdd8d44b36d69a6d4d1e67326fd1a7212173228
gray <40941118@gm.nfu.edu.tw>
2023-08-22 19:06:26 +0800

    加入admin Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminHomeController;
+use App\Http\Controllers\AdminPostsController;
+use App\Http\Controllers\OrmController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostsController;
@@ -16,7 +19,23 @@ use App\Http\Controllers\PostsController;
 */
 
 Route::get('/', [HomeController::class,'index']);
-
 Route::get('/posts', [PostsController::class,'index']);
-
 Route::get('/posts/{id}', [PostsController::class,'show']);
+
+Route::prefix('orm')->group(function () {
+    Route::get("/create", [OrmController::class, 'create']);
+    Route::get("/index", [OrmController::class, 'index']);
+    Route::get("/show/{id}", [OrmController::class, 'show']);
+    Route::get("/update/{id}", [OrmController::class, 'update']);
+    Route::get("/destroy/{id}", [OrmController::class, 'destroy']);
+});
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminHomeController::class, 'index'])->name("home.index");
+    Route::get('posts', [AdminPostsController::class, 'index'])->name("posts.index");
+    Route::get('posts/create', [AdminPostsController::class, 'create'])->name("posts.create");
+    Route::post('posts', [AdminPostsController::class, 'store'])->name("posts.store");
+    Route::get('posts/{post}/edit', [AdminPostsController::class, 'edit'])->name("posts.edit");
+    Route::patch('posts/{post}', [AdminPostsController::class, 'update'])->name("posts.update");
+    Route::delete('posts/{post}', [AdminPostsController::class, 'destroy'])->name("posts.destroy");
+});
