7dcefab12014a72350962dd57df489494dae04c5
gray <40941118@gm.nfu.edu.tw>
2023-08-22 19:10:24 +0800

    admin page

---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,34 @@
+@extends('admin.layouts.master')
+
+@section('page-title', 'Edit article')
+
+@section('page-content')
+<div class="container-fluid px-4">
+    <h1 class="mt-4">文章管理</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">編輯文章</li>
+    </ol>
+    <div class="alert alert-danger alert-dismissible" role="alert" id="liveAlert">
+        <strong>錯誤！</strong> 請修正以下問題：
+        <ul>
+            <li>錯誤 1</li>
+        </ul>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <form action="{{ route('admin.posts.update', $post->id) }}" method="POST">
+        @method('patch')
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">文章標題</label>
+            <input id="title" name="title" type="text" class="form-control" placeholder="請輸入文章標題" value="{{ $post->title }}">
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">文章內容</label>
+            <textarea id="content" name="content" class="form-control" rows="10" placeholder="請輸入文章內容">{{ $post->content }}</textarea>
+        </div>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <button type="submit" class="btn btn-primary btn-sm">儲存</button>
+        </div>
+    </form>
+</div>
+@endsection
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,44 @@
+@extends('admin.layouts.master')
+
+@section('page-title', 'Article list')
+
+@section('page-content')
+<div class="container-fluid px-4">
+    <h1 class="mt-4">文章管理</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">文章一覽表</li>
+    </ol>
+    <div class="alert alert-success alert-dismissible" role="alert" id="liveAlert">
+        <strong>完成！</strong> 成功儲存文章
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <a class="btn btn-success btn-sm" href="{{ route('admin.posts.create') }}">新增</a>
+    </div>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">標題</th>
+            <th scope="col">功能</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($posts as $post)
+            <tr>
+                <th scope="row" style="width: 50px">{{ $post->id }}</th>
+                <td>{{ $post->title }}</td>
+                <td style="width: 150px">
+                    <a class="btn btn-primary btn-sm" href="{{ route('admin.posts.edit', $post->id) }}">編輯</a>
+                    <form method="POST" action="{{ route('admin.posts.destroy', $post->id) }}" style="display: inline-block;">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-danger btn-sm">刪除</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
